1cae199c8fe919e753b15c3c2e55567a8b4309ac
yyymmp <j2538474647@163.com>
2021-12-05 22:02:31 +0800

    使用key 附件问题解决半包粘包问题

--- selfnetty/src/main/java/cn/itcast/nio/Client.java
+++ selfnetty/src/main/java/cn/itcast/nio/Client.java
@@ -3,6 +3,7 @@ package cn.itcast.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 /**
  * @author clearlove
@@ -15,6 +16,7 @@ public class Client {
     public static void main(String[] args) throws IOException {
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("localhost", 8080));
+        sc.write(Charset.defaultCharset().encode("1234567890qwert789456\n"));
 
         System.out.println("wait");
 
--- selfnetty/src/main/java/cn/itcast/nio/SelectServer.java
+++ selfnetty/src/main/java/cn/itcast/nio/SelectServer.java
@@ -1,5 +1,6 @@
 package cn.itcast.nio;
 
+import static cn.itcast.utils.ByteBufferUtil.debugAll;
 import static cn.itcast.utils.ByteBufferUtil.debugRead;
 
 import java.io.IOException;
@@ -25,6 +26,24 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class SelectServer {
+
+    private static void split(ByteBuffer buffer) {
+        for (int i = 0; i < buffer.limit(); i++) {
+            //找到\n
+            if (buffer.get(i) == '\n') {
+                //读出该条完整消息
+                int length = i + 1 - buffer.position();
+                ByteBuffer newBuf = ByteBuffer.allocate(length);
+                //从buffer读出 写入  newBuf
+                for (int j = 0; j < length; j++) {
+                    newBuf.put(buffer.get());
+                }
+                debugAll(newBuf);
+            }
+        }
+
+        buffer.compact();
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -69,6 +88,7 @@ public class SelectServer {
                 log.info("{}", key);
                 //区分事件类型
                 //该事件是不是连接事件
+
                 if (key.isAcceptable()) {
                     //因为现在只有连接事件 所以这个channel就是ServerSocketChannel
                     //这个key就是sscKey 并且多次连接始终只有一个
@@ -76,22 +96,37 @@ public class SelectServer {
                     //调用accept与客户端建立连接
                     SocketChannel socketChannel = channel.accept();
                     socketChannel.configureBlocking(false);
-                    SelectionKey selectionKey = socketChannel.register(selector, 0, null);
+                    //将buf作为附件关联到socketChannel上 
+                    ByteBuffer buffer = ByteBuffer.allocate(16);
+                    SelectionKey selectionKey = socketChannel.register(selector, 0, buffer);
                     selectionKey.interestOps(SelectionKey.OP_READ);
                 } else if (key.isReadable()) {
                     //拿到触发事件的channel
                     //发生读事件必然强转为socketChannel
                     try {
                         SocketChannel channel = (SocketChannel) key.channel();
-                        ByteBuffer buffer = ByteBuffer.allocate(4);
-                        int read = channel.read(buffer);
+                        //如果该buf不够接受一次的消息长度 会触发第二次read事件再次进行read
+                        //获取key的附件
+                        ByteBuffer attachment = ((ByteBuffer) key.attachment());
+                        int read = channel.read(attachment);
                         if (read == -1) {
                             //客户端正常断开
                             key.cancel();
                         } else {
-                            buffer.flip();
+                            attachment.flip();
+                            //这里需要处理消息边界
+                            split(attachment);
+
+                            if (attachment.position() == attachment.limit()) {
+                                //说明该条消息超过了buf容量 则扩容 新建buf
+                                ByteBuffer newBuf = ByteBuffer.allocate(attachment.capacity() * 2);
+                                attachment.flip();
+                                newBuf.put(attachment);
+                                //将扩容后的buf替换掉之前的buf
+                                key.attach(newBuf);
+                            }
                             //debugRead(buffer);
-                            System.out.println(Charset.defaultCharset().decode(buffer).toString());
+                            System.out.println(Charset.defaultCharset().decode(attachment).toString());
                         }
                     } catch (IOException ioException) {
                         //当客户端强制关闭时 会引发一个read操作 异常断开
