6187a312a648d6c77db301f0a22a1ba23eaac530
jializhong <jializhong@souche.com>
2022-01-13 09:36:28 +0800

    粘包半包演示

---
+++ selfnetty/src/main/java/cn/itcast/netty/protocol/Server.java
@@ -0,0 +1,45 @@
+package cn.itcast.netty.protocol;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import java.util.logging.Logger;
+
+/**
+ * @author jlz
+ * @date 2022年01月12日 22:39
+ */
+public class Server {
+
+    public static void main(String[] args) throws InterruptedException {
+        ServerBootstrap bootstrap = new ServerBootstrap()
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            //连接建立时触发
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                super.channelActive(ctx);
+
+                            }
+                        });
+                    }
+                });
+
+        ChannelFuture bind = bootstrap.bind(8081);
+        bind.sync();
+        bind.channel().closeFuture().sync();
+    }
+}
