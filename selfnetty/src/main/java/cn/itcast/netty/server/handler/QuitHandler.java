8f14eb314f7c81429af024d9d81d36497312bc8d
jializhong <jializhong@souche.com>
2022-02-08 14:37:22 +0800

    客户端正常退出与异常退出

---
+++ selfnetty/src/main/java/cn/itcast/netty/server/handler/QuitHandler.java
@@ -0,0 +1,34 @@
+package cn.itcast.netty.server.handler;
+
+import cn.itcast.netty.server.session.SessionFactory;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author jlz
+ * @date 2022年02月07日 23:41
+ */
+@Sharable
+@Slf4j
+public class QuitHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 关闭事件
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        SessionFactory.getSession().unbind(ctx.channel());
+        log.debug("{} 已经断开", ctx.channel());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        SessionFactory.getSession().unbind(ctx.channel());
+        log.debug("{} 已经异常断开 异常是{}", ctx.channel(), cause.getMessage());
+    }
+}
