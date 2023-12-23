736082e9701aefb012c3fb50b854eb491c4f0578
jializhong <jializhong@souche.com>
2023-12-23 19:44:19 +0800

    聊天室-读写空闲检测器

--- ershua/src/main/java/com/sup/netty/c4/server/ChatServer.java
+++ ershua/src/main/java/com/sup/netty/c4/server/ChatServer.java
@@ -8,13 +8,18 @@ import com.sup.netty.c4.server.handle.GroupCreateRequestMessageHandle;
 import com.sup.netty.c4.server.handle.LoginRequestMessageHandler;
 import com.sup.netty.c4.server.handle.QuitHandle;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -43,6 +48,24 @@ public class ChatServer {
                         @Override
                         protected void initChannel(NioSocketChannel ch) throws Exception {
                             ch.pipeline().addLast(
+                                    //连接假死检测
+                                    //参数1:读空闲 写空闲,如果不关心 则设为0
+                                    //参数2 写空闲 参数3 读或写
+                                    //若指令时间内为读到数据 则会触发事件IdleState#readIdle 如何处理该事件? 定义双向处理器中处理
+                                    new IdleStateHandler(5,0,0),
+                                    //同时作为出入站处理器 但这里不关心读写事件 只关心连接假死的特殊事件
+                                    new ChannelDuplexHandler(){
+                                        //专门响应特殊事件如读空闲事件IdleState#readIdle
+                                        @Override
+                                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                                            IdleStateEvent event = (IdleStateEvent) evt;
+                                            //事件时读空闲时间
+                                            if (event.state() == IdleState.READER_IDLE) {
+                                                log.error("读空闲已经5s未读到数据");
+                                            }
+                                        }
+                                    },
+
                                     //log只做打印 可共享
                                     new ProtocolFrameDecoder(),
                                     new LoggingHandler(LogLevel.DEBUG),
